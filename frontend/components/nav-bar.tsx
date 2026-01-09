32d335b1e641b557d029f3c1064a32140cb1afaf
OjasvaManik <manik.ojasva.3663@gmail.com>
2026-01-10 02:07:05 +0530

    Done 5

--- frontend/components/nav-bar.tsx
+++ frontend/components/nav-bar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ThemeToggle } from "./theme-toggle";
-import Link from "next/link";
 import TextType from "@/components/TextType";
 import { cn } from "@/lib/utils";
 import { bebasNeue } from "@/types/fonts";
@@ -8,7 +7,7 @@ import { bebasNeue } from "@/types/fonts";
 const NavBar = () => {
   return (
     <div className="relative flex w-full items-center justify-between">
-      <Link href={ '/' }>
+      <a href={ '/' }>
         <TextType
           text={ [ "Unitrix", "Note System" ] }
           typingSpeed={ 75 }
@@ -17,7 +16,7 @@ const NavBar = () => {
           cursorCharacter="_"
           className={ cn( bebasNeue.className, 'text-4xl lg:text-5xl uppercase' ) }
         />
-      </Link>
+      </a>
       <ThemeToggle/>
     </div>
   );
